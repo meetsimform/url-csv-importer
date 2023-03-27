28a40e043c901435441b8ebe18551e1a7ceb8f44
meetsimforrm <meet.h@simformsolutions.com>
2023-03-27 14:47:35 +0530

    code formatting

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # url-csv-importer
 - Below command needed to be run to import the urls from the csv
-php bin/console import-urls
+`php bin/console import-urls`
 
 - Following Sample csv file is located at 'public/uploads' direcrory with name :
-sample.csv
+`sample.csv`
--- src/Service/PageCsvImporter.php
+++ src/Service/PageCsvImporter.php
@@ -7,42 +7,42 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class PageCsvImporter
 {
-	const BATCH_UPLOAD_SIZE = 1000;
+    const BATCH_UPLOAD_SIZE = 1000;
 
-	private $em; 
+    private $em;
 
-	private $projectDir;
+    private $projectDir;
 
-	public function __construct($projectDir, EntityManagerInterface $em)
-	{
-		$this->projectDir = $projectDir;
+    public function __construct($projectDir, EntityManagerInterface $em)
+    {
+        $this->projectDir = $projectDir;
         $this->em = $em;
-	}
+    }
 
-	/**
-	 * @param integer $startLine
-	 * @param array   $headers
-	 * @return void 
-	 */
-	public function prepareProcessUrlImporter($startLine = 0, $headers = array())
-	{
-		$filePath = $this->projectDir . '/public/uploads/sample.csv';
-		$this->processUrlImporter($filePath, $startLine, $headers);
-	}
+    /**
+     * @param integer $startLine
+     * @param array   $headers
+     * @return void
+     */
+    public function prepareProcessUrlImporter($startLine = 0, $headers = [])
+    {
+        $filePath = $this->projectDir . "/public/uploads/sample.csv";
+        $this->processUrlImporter($filePath, $startLine, $headers);
+    }
 
-	/**
-	 * @param string  $startLine
-	 * @param integer $filePath
-	 * @param array   $headers
-	 * @return void 
-	 */
-	public function processUrlImporter($filePath, $startLine, $headers)
-	{
+    /**
+     * @param string  $startLine
+     * @param integer $filePath
+     * @param array   $headers
+     * @return void
+     */
+    public function processUrlImporter($filePath, $startLine, $headers)
+    {
         $file = new \SplFileObject($filePath, "r");
         $file->setFlags(\SplFileObject::SKIP_EMPTY);
         $file->seek($startLine);
 
-        $errorRows = $uniqueUrls = array();
+        $errorRows = $uniqueUrls = [];
         $count = $uploadCount = 0;
 
         while (!$file->eof() && $count < self::BATCH_UPLOAD_SIZE) {
@@ -50,62 +50,63 @@ class PageCsvImporter
             $lineNo = $count + $startLine;
             $pageDetails = $file->fgetcsv(",");
             $currentLine = $file->current();
-            $ignoreLine = $this->ignoreLine($currentLine, $pageDetails)	;
-            if ((null !== $pageDetails && $pageDetails) && !$ignoreLine) {
-                if(count($headers) == 0){
+            $ignoreLine = $this->ignoreLine($currentLine, $pageDetails);
+            if (null !== $pageDetails && $pageDetails && !$ignoreLine) {
+                if (count($headers) == 0) {
                     $headers = $pageDetails;
                     continue;
                 }
                 $pageData = array_combine($headers, $pageDetails);
-                $url = isset($pageData['url']) ? $pageData['url'] : '';
+                $url = isset($pageData["url"]) ? $pageData["url"] : "";
                 // validation if emplty url string is passed
-                if(empty($url)) {
+                if (empty($url)) {
                     continue;
                 }
                 // validation if url is invalid
-                if (isset($pageData['url']) && trim($pageData['url'])) {
-                    $isValidUrl =  filter_var($url, FILTER_VALIDATE_URL);
+                if (isset($pageData["url"]) && trim($pageData["url"])) {
+                    $isValidUrl = filter_var($url, FILTER_VALIDATE_URL);
                     if (!$isValidUrl) {
-                      	continue;
+                        continue;
                     }
                 }
                 // validation if duplicate urls is passed in a csv
-                if (isset($pageData['url']) && trim($pageData['url'])) {
-                    if (!isset($uniqueUrls[trim($pageData['url'])])) {
-                        $uniqueUrls[trim($pageData['url'])] = true;
+                if (isset($pageData["url"]) && trim($pageData["url"])) {
+                    if (!isset($uniqueUrls[trim($pageData["url"])])) {
+                        $uniqueUrls[trim($pageData["url"])] = true;
                     } else {
                         continue;
                     }
                 }
                 try {
-                	// check if url already exists
-                	$page = $this->em->getRepository(Page::class)->findBy(['url' => $url]);
-                	if (!$page) {
-                        $page = new Page;
+                    // check if url already exists
+                    $page = $this->em
+                        ->getRepository(Page::class)
+                        ->findBy(["url" => $url]);
+                    if (!$page) {
+                        $page = new Page();
                         $page->setUrl($url);
                         $page->setCreatedAt(new \DateTimeImmutable());
                         $uploadCount++;
                         $this->em->persist($page);
-                	}
-                }catch(\Exception $e){
-                    
+                    }
+                } catch (\Exception $e) {
                 }
             }
         }
         $this->em->flush();
 
         if (!$file->eof()) {
-        	$startLine = ($startLine + $count) -1;
-        	$this->prepareProcessUrlImporter($startLine, $headers);
+            $startLine = $startLine + $count - 1;
+            $this->prepareProcessUrlImporter($startLine, $headers);
         }
-	}
-	
-	/**
-	 * @param string $line
-	 * @param array  $csvArray
-	 * @return boolean
-	 */
-	private function ignoreLine($line, $csvArray)
+    }
+
+    /**
+     * @param string $line
+     * @param array  $csvArray
+     * @return boolean
+     */
+    private function ignoreLine($line, $csvArray)
     {
         if (strlen(trim($line)) == 0) {
             return true;
@@ -113,13 +114,17 @@ class PageCsvImporter
         $firstChar = substr($line, 0, 1);
         $firstTwoChars = substr($line, 0, 2);
         if (!$csvArray) {
-            $csvArray = array();
+            $csvArray = [];
         } else {
             $csvArray = array_filter($csvArray);
         }
 
-        if ($firstChar == "#" || $firstTwoChars == "\"#" || trim($line) == ""
-                || count($csvArray) == 0) {
+        if (
+            $firstChar == "#" ||
+            $firstTwoChars == "\"#" ||
+            trim($line) == "" ||
+            count($csvArray) == 0
+        ) {
             return true;
         }
         return false;
